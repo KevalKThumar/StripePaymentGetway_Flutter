0468f4a8c809e3587caf9ac5250252e2902c1343
KevalKThumar <thumarkeval12@gmail.com>
2024-06-14 00:01:32 +0530

    project setup for stripe payment getway

---
+++ android/app/src/main/kotlin/com/example/prectical_exam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prectical_exam
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity: FlutterFragmentActivity() {
+}
